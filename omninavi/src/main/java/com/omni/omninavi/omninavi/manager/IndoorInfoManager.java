d412b7f160e50700131abeaf3700a55e5029d4bd
Wiliiam Wang <william.wang@omniguider.com>
2017-12-04 15:51:40 +0800

    add poi icons and navi api

--- omninavi/src/main/java/com/omni/omninavi/omninavi/manager/IndoorInfoManager.java
+++ omninavi/src/main/java/com/omni/omninavi/omninavi/manager/IndoorInfoManager.java
@@ -3,11 +3,16 @@ package com.omni.omninavi.omninavi.manager;
 import android.content.Context;
 
 import com.android.volley.Request;
+import com.android.volley.VolleyError;
 import com.omni.omninavi.omninavi.model.OGBuilding;
 import com.omni.omninavi.omninavi.model.OGFloors;
+import com.omni.omninavi.omninavi.model.OGNaviRoute;
+import com.omni.omninavi.omninavi.model.OGNaviRoutePOI;
+import com.omni.omninavi.omninavi.model.google_navigation.GoogleNavigationRoute;
 import com.omni.omninavi.omninavi.tool.DialogTools;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -17,6 +22,8 @@ import java.util.Map;
 public class IndoorInfoManager {
 
     private static IndoorInfoManager sIndoorManager;
+
+    private final int TIMEOUT = 15000;
 
     public static IndoorInfoManager create() {
         if (sIndoorManager == null) {
@@ -45,6 +52,128 @@ public class IndoorInfoManager {
         params.put("b", "2");
 
         NetworkManager.getInstance().addJsonRequest(context, Request.Method.GET, url, params, OGFloors.class, NetworkManager.DEFAULT_TIMEOUT, listener);
+    }
+
+    public void getIndoorPToPRoute(Context context, String startPOIId, String endPOIId, NetworkManager.NetworkManagerListener<OGNaviRoute> listener) {
+        DialogTools.getInstance().showProgress(context);
+
+        String url = NetworkManager.DOMAIN_NAME + "locapi/get_navi_route";
+        Map<String, String> params = new HashMap<>();
+        params.put("a", startPOIId);
+        params.put("b", endPOIId);
+
+        NetworkManager.getInstance().addJsonRequest(context, Request.Method.GET, url, params, OGNaviRoute.class, TIMEOUT, listener);
+    }
+
+    public void getOutdoorToIndoorPRoute(Context context, String pOIId, double userLat, double userLng, String userCurrentFloor, NetworkManager.NetworkManagerListener<OGNaviRoute> listener) {
+        DialogTools.getInstance().showProgress(context);
+
+        String url = NetworkManager.DOMAIN_NAME + "locapi/get_navi_route_xy";
+        Map<String, String> params = new HashMap<>();
+        params.put("p", pOIId);
+        params.put("lat", String.valueOf(userLat));
+        params.put("lng", String.valueOf(userLng));
+        params.put("f", userCurrentFloor);
+
+        NetworkManager.getInstance().addJsonRequest(context, Request.Method.GET, url, params, OGNaviRoute.class, TIMEOUT, listener);
+    }
+
+    public void getIndoorUserLocationToExit(Context context, String currentBuildingId, String userCurrentFloorNumber,
+                                            String userLat, String userLng, String exitLat, String exitLng,
+                                            NetworkManager.NetworkManagerListener<OGNaviRoute> listener) {
+        DialogTools.getInstance().showProgress(context);
+
+        String url = NetworkManager.DOMAIN_NAME + "locapi/get_navi_xytopoi";
+        Map<String, String> params = new HashMap<>();
+        params.put("b", currentBuildingId);
+        params.put("f", userCurrentFloorNumber);
+        params.put("a_lat", userLat);// 25.xxx
+        params.put("a_lng", userLng);// 121.xxx
+        params.put("b_lat", exitLat);
+        params.put("b_lng", exitLng);
+
+        NetworkManager.getInstance().addJsonRequest(context, Request.Method.GET, url, params, OGNaviRoute.class, TIMEOUT, listener);
+    }
+
+    public void getIndoorUserLocationToExitWithFloorPlanId(Context context, String poiId,
+                                                           double userLat, double userLng, String floorPlanId,
+                                                           NetworkManager.NetworkManagerListener<OGNaviRoute> listener) {
+        DialogTools.getInstance().showProgress(context);
+
+        String url = NetworkManager.DOMAIN_NAME + "locapi/get_navi_routexy_planid";
+        Map<String, String> params = new HashMap<>();
+        params.put("p", poiId);
+        params.put("lat", userLat + "");
+        params.put("lng", userLng + "");
+        params.put("planid", floorPlanId);
+
+        NetworkManager.getInstance().addJsonRequest(context, Request.Method.GET, url, params, OGNaviRoute.class, TIMEOUT, listener);
+    }
+
+    public void getUserIndoorLocationToOutdoorPRoute(final Context context,
+                                                     final double userLat, final double userLng,
+                                                     double outdoorPOILat, double outdoorPOILng,
+                                                     final String currentBuildingId, final String userCurrentFloorNumber,
+                                                     final NetworkManager.NetworkManagerListener<OGNaviRoute> listener) {
+        DialogTools.getInstance().showProgress(context);
+
+        String output = "json";
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output;
+        Map<String, String> params = new HashMap<>();
+        params.put("origin", userLat + "," + userLng);
+        params.put("destination", outdoorPOILat + "," + outdoorPOILng);
+        params.put("sensor", "false");
+
+        NetworkManager.getInstance().addJsonRequest(context, Request.Method.GET, url, params, GoogleNavigationRoute.class, TIMEOUT, new NetworkManager.NetworkManagerListener<GoogleNavigationRoute>() {
+            @Override
+            public void onSucceed(GoogleNavigationRoute response) {
+                if (response.getStatus().equals(GoogleNavigationRoute.GET_ROUTE_STATUS_OK)) {
+
+                    final List<OGNaviRoutePOI> poiList = response.getNavigationRoute().getNavigationalRoutePOIs();
+                    if (poiList != null && !poiList.isEmpty()) {
+                        OGNaviRoutePOI poi = poiList.get(0);
+
+                        getIndoorUserLocationToExit(context,
+                                currentBuildingId, userCurrentFloorNumber,
+                                String.valueOf(userLat), String.valueOf(userLng),
+                                String.valueOf(poi.getLatitude()), String.valueOf(poi.getLongitude()),
+                                new NetworkManager.NetworkManagerListener<OGNaviRoute>() {
+                                    @Override
+                                    public void onSucceed(OGNaviRoute navigationalRoute) {
+                                        if (navigationalRoute.getResult().equals("true")) {
+                                            navigationalRoute.getNavigationalRoutePOIs().addAll(poiList);
+                                        }
+                                        listener.onSucceed(navigationalRoute);
+                                    }
+
+                                    @Override
+                                    public void onFail(VolleyError error, boolean shouldRetry) {
+                                        listener.onFail(error, shouldRetry);
+                                    }
+                                });
+                    }
+                }
+            }
+
+            @Override
+            public void onFail(VolleyError error, boolean shouldRetry) {
+
+            }
+        });
+    }
+
+    public void getEmergencyRoute(Context context, String buildingId, double userLat, double userLng, String userCurrentFloor, String type, NetworkManager.NetworkManagerListener<OGNaviRoute> listener) {
+        DialogTools.getInstance().showProgress(context);
+
+        String url = NetworkManager.DOMAIN_NAME + "locapi/get_navi_nearby_type";
+        Map<String, String> params = new HashMap<>();
+        params.put("b", buildingId);
+        params.put("f", userCurrentFloor);
+        params.put("lat", String.valueOf(userLat));
+        params.put("lng", String.valueOf(userLng));
+        params.put("type", type);
+
+        NetworkManager.getInstance().addJsonRequest(context, Request.Method.GET, url, params, OGNaviRoute.class, TIMEOUT, listener);
     }
 
 }
--- omninavi/src/main/java/com/omni/omninavi/omninavi/model/OGPOI.java
+++ omninavi/src/main/java/com/omni/omninavi/omninavi/model/OGPOI.java
@@ -1,6 +1,7 @@
 package com.omni.omninavi.omninavi.model;
 
 import com.google.gson.annotations.SerializedName;
+import com.omni.omninavi.R;
 
 import java.io.Serializable;
 
@@ -10,30 +11,24 @@ import java.io.Serializable;
 
 public class OGPOI implements Serializable {
 
-    public static final int TYPE_STAIR = 1;
-    public static final int TYPE_ENTRANCE = 2;
-    public static final int TYPE_ELEVATOR = 3;
-    //    public static final int TYPE_ROOM = 4;
-    public static final int TYPE_OFFICE = 5;
-    public static final int TYPE_TOILETS = 6;
-    public static final int TYPE_DISABLED_TOILETS = 7;
-    public static final int TYPE_FIRE_EXTINGUISHER = 8;
-    public static final int TYPE_AED = 9;
-    public static final int TYPE_RAMP = 10;
-    public static final int TYPE_SECURITY_GUARD = 11;
-    //    public static final int TYPE_KITCHEN = 12;
-    public static final int TYPE_OTHER = 13;
-    public static final int TYPE_EXHIBIT = 14;
-    public static final int TYPE_FIRE_HYDRANT = 15;
-    public static final int TYPE_INFORMATION = 16;
-    public static final int TYPE_DINING = 17;
-    public static final int TYPE_EMERGENCY_EXIT = 18;
-    public static final int TYPE_ESCAPE_SLING = 19;
-    public static final int TYPE_MEETING_ROOM = 20;
-    public static final int TYPE_INTRODUCE = 21;
-    public static final int TYPE_RESTROOM = 22;
-    public static final int TYPE_POST_OFFICE = 23;
-    public static final int TYPE_ATM = 24;
+    public static final int TYPE_ELEVATOR = 1;//    Elevator
+    public static final int TYPE_STAIR = 2;//            Stair
+    public static final int TYPE_RESTROOM = 3;//    Restroom
+    public static final int TYPE_RESTROOM_FOR_THE_DISABLED = 4;//    Restroom or the Disabled
+    public static final int TYPE_AED = 5;//    AED
+    public static final int TYPE_ENTRANCE_OR_EXIT = 6;//    Entrance/Exit
+    public static final int TYPE_POST_OFFICE = 7;//    Post Office
+    public static final int TYPE_ATM = 8;//    ATM
+    public static final int TYPE_CONVENIENCE_STORE = 9;//    Convenience Store
+    public static final int TYPE_DIVISION = 10;//    Division
+    public static final int TYPE_HYDRANT = 11;//            Hydrant
+    public static final int TYPE_RESTAURANT = 12;//    Restaurant
+    public static final int TYPE_BANK = 13;//            Bank
+    public static final int TYPE_ESCALATOR = 14;//    Escalator
+    public static final int TYPE_INFORMATION = 15;//            Information
+    public static final int TYPE_DRINKING_FOUNTAINS = 16;//    Drinking fountains
+    public static final int TYPE_FEEDING_ROOM = 17;//    Breastfeeding Room
+    public static final int TYPE_MEETING_ROOM = 18;//    Meeting Room
 
     @SerializedName("id")
     private String id;
@@ -43,6 +38,11 @@ public class OGPOI implements Serializable {
     private String desc;
     @SerializedName("type")
     private String type;
+    @SerializedName("type_zh")
+    private String typeZh;
+    @SerializedName("icon")
+    private OGPOIIcon ogPoiIcon;
+
     @SerializedName("lat")
     private double latitude;
     @SerializedName("lng")
@@ -51,20 +51,11 @@ public class OGPOI implements Serializable {
     private String isEntrance;
     @SerializedName("is_door")
     private String isDoor;
-    @SerializedName("is_exhibit")
-    private String isExhibit;
-//    @SerializedName("exhibit")
-//    private Exhibit[] exhibits;
-    @SerializedName("type_zh")
-    private String typeZh;
+    @SerializedName("is_office")
+    private boolean isOffice;
+    @SerializedName("office")
+    private OGOffice[] ogOffices;
 
-    /**
-     * image & url key not exist in json
-     */
-    @SerializedName("image")
-    private String urlToPoisImage;
-    @SerializedName("url")
-    private String url;
     private int poiType = -1;
     private boolean isOriginalPOI = false;
 
@@ -99,16 +90,12 @@ public class OGPOI implements Serializable {
     public int getPOIType() {
         if (type != null) {
             switch (type) {
-                case "Stair":
-                    poiType = TYPE_STAIR;
-                    break;
-
-//                case "Entrance":
-//                    poiType = TYPE_ENTRANCE;
-//                    break;
-//
                 case "Elevator":
                     poiType = TYPE_ELEVATOR;
+                    break;
+
+                case "Stair":
+                    poiType = TYPE_STAIR;
                     break;
 
                 case "Restroom":
@@ -116,15 +103,15 @@ public class OGPOI implements Serializable {
                     break;
 
                 case "Restroom or the Disabled":
-                    poiType = TYPE_DISABLED_TOILETS;
+                    poiType = TYPE_RESTROOM_FOR_THE_DISABLED;
                     break;
 
                 case "AED":
                     poiType = TYPE_AED;
                     break;
 
-                case "Entrance\\/Exit":
-                    poiType = TYPE_ENTRANCE;
+                case "Entrance/Exit":
+                    poiType = TYPE_ENTRANCE_OR_EXIT;
                     break;
 
                 case "Post Office":
@@ -136,102 +123,45 @@ public class OGPOI implements Serializable {
                     break;
 
                 case "Convenience Store":
+                    poiType = TYPE_CONVENIENCE_STORE;
                     break;
 
                 case "Division":
+                    poiType = TYPE_DIVISION;
                     break;
 
                 case "Hydrant":
-                    poiType = TYPE_FIRE_HYDRANT;
+                    poiType = TYPE_HYDRANT;
                     break;
 
                 case "Restaurant":
+                    poiType = TYPE_RESTAURANT;
                     break;
 
                 case "Bank":
+                    poiType = TYPE_BANK;
                     break;
 
                 case "Escalator":
+                    poiType = TYPE_ESCALATOR;
                     break;
 
                 case "Information":
+                    poiType = TYPE_INFORMATION;
                     break;
 
                 case "Drinking fountains":
+                    poiType = TYPE_DRINKING_FOUNTAINS;
                     break;
 
-////                case "Room":
-////                    poiType = TYPE_ROOM;
-////                    break;
-//
-//                case "Office":
-//                    poiType = TYPE_OFFICE;
-//                    break;
-//
-//                case "Toilets":
-//                case "友善廁所":
-//                    poiType = TYPE_TOILETS;
-//                    break;
-//
-//                case "Disabled Toilets":
-//                    poiType = TYPE_DISABLED_TOILETS;
-//                    break;
-//
-//                case "Fire Extinguisher":
-//                    poiType = TYPE_FIRE_EXTINGUISHER;
-//                    break;
-//
-//                case "First Aid/AED":
-//                    poiType = TYPE_AED;
-//                    break;
-//
-//                case "Ramp":
-//                    poiType = TYPE_RAMP;
-//                    break;
-//
-//                case "Security/Guard":
-//                    poiType = TYPE_SECURITY_GUARD;
-//                    break;
-//
-////                case "Kitchen":
-////                    poiType = TYPE_KITCHEN;
-////                    break;
-//
-//                case "Other":
-//                    poiType = TYPE_OTHER;
-//                    break;
-//
-//                case "exhibits":
-//                    poiType = TYPE_EXHIBIT;
-//                    break;
-//
-//                case "firehydrant":
-//                    poiType = TYPE_FIRE_HYDRANT;
-//                    break;
-//
-//                case "information":
-//                    poiType = TYPE_INFORMATION;
-//                    break;
-//
-//                case "dining":
-//                    poiType = TYPE_DINING;
-//                    break;
-//
-//                case "emergency_exit":
-//                    poiType = TYPE_EMERGENCY_EXIT;
-//                    break;
-//
-//                case "escapesling":
-//                    poiType = TYPE_ESCAPE_SLING;
-//                    break;
-//
-//                case "meetingroom":
-//                    poiType = TYPE_MEETING_ROOM;
-//                    break;
-//
-//                case "introduce":
-//                    poiType = TYPE_INTRODUCE;
-//                    break;
+                case "Breastfeeding Room":
+                    poiType = TYPE_FEEDING_ROOM;
+                    break;
+
+                case "Meeting Room":
+                    poiType = TYPE_MEETING_ROOM;
+                    break;
+
             }
         }
 
@@ -254,22 +184,6 @@ public class OGPOI implements Serializable {
         this.longitude = longitude;
     }
 
-    public String getUrlToPoisImage() {
-        return urlToPoisImage;
-    }
-
-    public void setUrlToPoisImage(String urlToPoisImage) {
-        this.urlToPoisImage = urlToPoisImage;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
     public String getIsEntrance() {
         return isEntrance;
     }
@@ -286,22 +200,6 @@ public class OGPOI implements Serializable {
         this.isDoor = isDoor;
     }
 
-    public String getIsExhibit() {
-        return isExhibit;
-    }
-
-    public void setIsExhibit(String isExhibit) {
-        this.isExhibit = isExhibit;
-    }
-
-//    public Exhibit[] getExhibits() {
-//        return exhibits;
-//    }
-//
-//    public void setExhibits(Exhibit[] exhibits) {
-//        this.exhibits = exhibits;
-//    }
-
     public String getTypeZh() {
         return typeZh;
     }
@@ -310,87 +208,87 @@ public class OGPOI implements Serializable {
         this.typeZh = typeZh;
     }
 
-//    public int getPOIIconRes(boolean isSelected) {
-//        int resId;
-//        switch (getPOIType()) {
-//            case OGPOI.TYPE_OTHER:
-//                resId = isSelected ? R.mipmap.icon_select : R.mipmap.poi_icon_other;
-//                break;
-//
-////            case OGPOI.TYPE_KITCHEN:
-////                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_dining;
-////                break;
-//
-//            case OGPOI.TYPE_OFFICE:
-//                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_office;
-//                break;
-//
-//            case OGPOI.TYPE_TOILETS:
-//                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_toilet;
-//                break;
-//
-//            case OGPOI.TYPE_DISABLED_TOILETS:
-//                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_toilet;
-//                break;
-//
-//            case OGPOI.TYPE_ELEVATOR:
-//                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_elevator;
-//                break;
-//
-//            case OGPOI.TYPE_ENTRANCE:
-//                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_emergency_exit;
-//                break;
-//
-//            case OGPOI.TYPE_STAIR:
-//                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_stairs;
-//                break;
-//
-//            case OGPOI.TYPE_EXHIBIT:
-//                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_expo;
-//                break;
-//
-//            case OGPOI.TYPE_FIRE_EXTINGUISHER:
-//                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_extinguisher;
-//                break;
-//
-//            case OGPOI.TYPE_AED:
-//                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_aed;
-//                break;
-//
-//            case OGPOI.TYPE_FIRE_HYDRANT:
-//                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_hydrant;
-//                break;
-//
-//            case OGPOI.TYPE_INFORMATION:
-//                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_info;
-//                break;
-//
-//            case OGPOI.TYPE_DINING:
-//                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_dining;
-//                break;
-//
-//            case OGPOI.TYPE_EMERGENCY_EXIT:
-//                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_emergency_exit;
-//                break;
-//
-//            case OGPOI.TYPE_ESCAPE_SLING:
-//                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_escape_sling;
-//                break;
-//
-//            case OGPOI.TYPE_MEETING_ROOM:
-//                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_meeting_room;
-//                break;
-//
-//            case OGPOI.TYPE_INTRODUCE:
-//                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_introduce;
-//                break;
-//
-//            default:
-//                resId = isSelected ? R.mipmap.poi_icon_other_highlight : R.mipmap.poi_icon_other;
-//                break;
-//        }
-//        return resId;
-//    }
+    public int getPOIIconRes(boolean isSelected) {
+        int resId;
+        switch (getPOIType()) {
+            case OGPOI.TYPE_ELEVATOR:
+                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_elevator;
+                break;
+
+            case OGPOI.TYPE_STAIR:
+                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_stairs;
+                break;
+
+            case OGPOI.TYPE_RESTROOM:
+                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_toilet;
+                break;
+
+            case OGPOI.TYPE_RESTROOM_FOR_THE_DISABLED:
+                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_restroom_for_the_disable;
+                break;
+
+            case OGPOI.TYPE_AED:
+                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_aed;
+                break;
+
+            case OGPOI.TYPE_ENTRANCE_OR_EXIT:
+                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_emergency_exit;
+                break;
+
+            case OGPOI.TYPE_POST_OFFICE:
+                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_post;
+                break;
+
+            case OGPOI.TYPE_ATM:
+                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_atm;
+                break;
+
+            case OGPOI.TYPE_CONVENIENCE_STORE:
+                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_ok;
+                break;
+
+            case OGPOI.TYPE_DIVISION:
+                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_tpe_gov;
+                break;
+
+            case OGPOI.TYPE_HYDRANT:
+                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_hydrant;
+                break;
+
+            case OGPOI.TYPE_RESTAURANT:
+                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_dining;
+                break;
+
+            case OGPOI.TYPE_BANK:
+                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_fubon;
+                break;
+
+            case OGPOI.TYPE_ESCALATOR:
+                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_escalator;
+                break;
+
+            case OGPOI.TYPE_INFORMATION:
+                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_information;
+                break;
+
+            case OGPOI.TYPE_DRINKING_FOUNTAINS:
+                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_drinking_fountain;
+                break;
+
+            case OGPOI.TYPE_FEEDING_ROOM:
+                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_feeding_room;
+                break;
+
+            case OGPOI.TYPE_MEETING_ROOM:
+                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_meeting_room;
+                break;
+
+            default:
+                resId = isSelected ? R.mipmap.icon_select : R.mipmap.icon_select;
+                break;
+        }
+        return resId;
+    }
 
     public boolean isOriginalPOI() {
         return isOriginalPOI;
@@ -400,33 +298,29 @@ public class OGPOI implements Serializable {
         isOriginalPOI = originalPOI;
     }
 
-    public boolean isExhibitPOI() {
-        return getPOIType() == OGPOI.TYPE_EXHIBIT;
-    }
-
-    public boolean isUtilityPOI() {
-        return getPOIType() == OGPOI.TYPE_STAIR ||
-                getPOIType() == OGPOI.TYPE_ELEVATOR ||
-//                getPOIType() == OGPOI.TYPE_ROOM ||
-                getPOIType() == OGPOI.TYPE_OFFICE ||
-                getPOIType() == OGPOI.TYPE_TOILETS ||
-                getPOIType() == OGPOI.TYPE_DISABLED_TOILETS ||
-                getPOIType() == OGPOI.TYPE_RAMP ||
-//                getPOIType() == OGPOI.TYPE_KITCHEN ||
-                getPOIType() == OGPOI.TYPE_MEETING_ROOM ||
-                getPOIType() == OGPOI.TYPE_INFORMATION ||
-                getPOIType() == OGPOI.TYPE_INTRODUCE ||
-                getPOIType() == OGPOI.TYPE_DINING ||
-                getPOIType() == OGPOI.TYPE_OTHER;
-    }
-
-    public boolean isEmergencyPOI() {
-        return getPOIType() == OGPOI.TYPE_AED ||
-                getPOIType() == OGPOI.TYPE_FIRE_EXTINGUISHER ||
-                getPOIType() == OGPOI.TYPE_SECURITY_GUARD ||
-                getPOIType() == OGPOI.TYPE_EMERGENCY_EXIT ||
-                getPOIType() == OGPOI.TYPE_FIRE_HYDRANT ||
-                getPOIType() == OGPOI.TYPE_ESCAPE_SLING ||
-                getPOIType() == OGPOI.TYPE_ENTRANCE;
-    }
+//    public boolean isUtilityPOI() {
+//        return getPOIType() == OGPOI.TYPE_STAIR ||
+//                getPOIType() == OGPOI.TYPE_ELEVATOR ||
+////                getPOIType() == OGPOI.TYPE_ROOM ||
+//                getPOIType() == OGPOI.TYPE_OFFICE ||
+//                getPOIType() == OGPOI.TYPE_TOILETS ||
+//                getPOIType() == OGPOI.TYPE_DISABLED_TOILETS ||
+//                getPOIType() == OGPOI.TYPE_RAMP ||
+////                getPOIType() == OGPOI.TYPE_KITCHEN ||
+//                getPOIType() == OGPOI.TYPE_MEETING_ROOM ||
+//                getPOIType() == OGPOI.TYPE_INFORMATION ||
+//                getPOIType() == OGPOI.TYPE_INTRODUCE ||
+//                getPOIType() == OGPOI.TYPE_DINING ||
+//                getPOIType() == OGPOI.TYPE_OTHER;
+//    }
+//
+//    public boolean isEmergencyPOI() {
+//        return getPOIType() == OGPOI.TYPE_AED ||
+//                getPOIType() == OGPOI.TYPE_FIRE_EXTINGUISHER ||
+//                getPOIType() == OGPOI.TYPE_SECURITY_GUARD ||
+//                getPOIType() == OGPOI.TYPE_EMERGENCY_EXIT ||
+//                getPOIType() == OGPOI.TYPE_FIRE_HYDRANT ||
+//                getPOIType() == OGPOI.TYPE_ESCAPE_SLING ||
+//                getPOIType() == OGPOI.TYPE_ENTRANCE;
+//    }
 }
